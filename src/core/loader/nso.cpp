a1199ddd48e3f0351948cc430d9299aa27c2165c
mailwl <mailwl@gmail.com>
2018-06-04 14:05:13 +0300

    Loader/NSO: Update NsoHeader according switchbrew

--- src/core/loader/nso.cpp
+++ src/core/loader/nso.cpp
@@ -20,23 +20,35 @@ namespace Loader {
 struct NsoSegmentHeader {
     u32_le offset;
     u32_le location;
-    u32_le size;
-    union {
-        u32_le alignment;
-        u32_le bss_size;
-    };
+    u32_le decompressed_size;
 };
-static_assert(sizeof(NsoSegmentHeader) == 0x10, "NsoSegmentHeader has incorrect size.");
+static_assert(sizeof(NsoSegmentHeader) == 0x0C, "NsoSegmentHeader has incorrect size.");
+
+struct Extent {
+    u32_le region_ro_offset;
+    u32_le region_size;
+};
 
 struct NsoHeader {
     u32_le magic;
-    INSERT_PADDING_BYTES(0xc);
-    std::array<NsoSegmentHeader, 3> segments; // Text, RoData, Data (in that order)
+    u32_le version;
+    u32_le reserved;
+    u32_le flags;
+    NsoSegmentHeader text;
+    u32_le module_name_offset;
+    NsoSegmentHeader rodata;
+    u32_le module_name_size;
+    NsoSegmentHeader data;
     u32_le bss_size;
-    INSERT_PADDING_BYTES(0x1c);
+    u8 build_id[0x20];
     std::array<u32_le, 3> segments_compressed_size;
+    INSERT_PADDING_BYTES(0x1C);
+    Extent api_info;
+    Extent dynstr;
+    Extent dynsym;
+    std::array<std::array<u8, 0x20>, 3> sha256_hashes;
 };
-static_assert(sizeof(NsoHeader) == 0x6c, "NsoHeader has incorrect size.");
+static_assert(sizeof(NsoHeader) == 0x100, "NsoHeader has incorrect size.");
 
 struct ModHeader {
     u32_le magic;
@@ -67,7 +79,7 @@ FileType AppLoader_NSO::IdentifyType(FileUtil::IOFile& file, const std::string&)
 }
 
 static std::vector<u8> ReadSegment(FileUtil::IOFile& file, const NsoSegmentHeader& header,
-                                   int compressed_size) {
+                                   size_t compressed_size) {
     std::vector<u8> compressed_data;
     compressed_data.resize(compressed_size);
 
@@ -78,13 +90,16 @@ static std::vector<u8> ReadSegment(FileUtil::IOFile& file, const NsoSegmentHeade
     }
 
     std::vector<u8> uncompressed_data;
-    uncompressed_data.resize(header.size);
-    const int bytes_uncompressed = LZ4_decompress_safe(
-        reinterpret_cast<const char*>(compressed_data.data()),
-        reinterpret_cast<char*>(uncompressed_data.data()), compressed_size, header.size);
+    uncompressed_data.resize(header.decompressed_size);
+    const int bytes_uncompressed =
+        LZ4_decompress_safe(reinterpret_cast<const char*>(compressed_data.data()),
+                            reinterpret_cast<char*>(uncompressed_data.data()), compressed_size,
+                            header.decompressed_size);
 
-    ASSERT_MSG(bytes_uncompressed == header.size && bytes_uncompressed == uncompressed_data.size(),
-               "{} != {} != {}", bytes_uncompressed, header.size, uncompressed_data.size());
+    ASSERT_MSG(bytes_uncompressed == header.decompressed_size &&
+                   bytes_uncompressed == uncompressed_data.size(),
+               "{} != {} != {}", bytes_uncompressed, header.decompressed_size,
+               uncompressed_data.size());
 
     return uncompressed_data;
 }
@@ -112,13 +127,13 @@ VAddr AppLoader_NSO::LoadModule(const std::string& path, VAddr load_base) {
     // Build program image
     Kernel::SharedPtr<Kernel::CodeSet> codeset = Kernel::CodeSet::Create("");
     std::vector<u8> program_image;
-    for (int i = 0; i < nso_header.segments.size(); ++i) {
-        std::vector<u8> data =
-            ReadSegment(file, nso_header.segments[i], nso_header.segments_compressed_size[i]);
-        program_image.resize(nso_header.segments[i].location);
+    std::array<NsoSegmentHeader, 3> segments{nso_header.text, nso_header.rodata, nso_header.data};
+    for (u32 i = 0; i < segments.size(); ++i) {
+        auto data = ReadSegment(file, segments[i], nso_header.segments_compressed_size[i]);
+        program_image.resize(segments[i].location);
         program_image.insert(program_image.end(), data.begin(), data.end());
-        codeset->segments[i].addr = nso_header.segments[i].location;
-        codeset->segments[i].offset = nso_header.segments[i].location;
+        codeset->segments[i].addr = segments[i].location;
+        codeset->segments[i].offset = segments[i].location;
         codeset->segments[i].size = PageAlignSize(static_cast<u32>(data.size()));
     }
 
@@ -129,7 +144,7 @@ VAddr AppLoader_NSO::LoadModule(const std::string& path, VAddr load_base) {
     // Read MOD header
     ModHeader mod_header{};
     // Default .bss to size in segment header if MOD0 section doesn't exist
-    u32 bss_size{PageAlignSize(nso_header.segments[2].bss_size)};
+    u32 bss_size{PageAlignSize(nso_header.bss_size)};
     std::memcpy(&mod_header, program_image.data() + module_offset, sizeof(ModHeader));
     const bool has_mod_header{mod_header.magic == Common::MakeMagic('M', 'O', 'D', '0')};
     if (has_mod_header) {
